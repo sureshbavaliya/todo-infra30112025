2c48dc641abd827b01b266aa8e644f52011d988c
Suresh Bavaliya <suresh.bavaliya565@gmail.com>
2025-12-18 20:54:12 +0530

    Updated

--- modules/azurerm_compute/data.tf
+++ modules/azurerm_compute/data.tf
@@ -2,7 +2,7 @@ data "azurerm_virtual_network" "vnet" {
   for_each = var.vms
 
   name                = each.value.vnet_name
-  resource_group_name = each.value.resource_group_name   # ✅ FIX
+  resource_group_name = each.value.resource_group_name   
 }
 
 data "azurerm_subnet" "frontend" {
@@ -10,7 +10,7 @@ data "azurerm_subnet" "frontend" {
 
   name                 = each.value.subnet_name
   virtual_network_name = data.azurerm_virtual_network.vnet[each.key].name
-  resource_group_name  = each.value.resource_group_name   # ✅ FIX
+  resource_group_name  = each.value.resource_group_name  
 
 }
 
